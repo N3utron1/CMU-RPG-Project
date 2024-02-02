6022c8503b9b24c2b3343e96e550cde35b565cb4
Louis Jankowski <louisjankowski@gmail.com>
2024-02-02 11:38:13 -0500

    add Player
    added character movement with platform bugs

---
+++ CMU RPG Demo/Assets/Scripts/PKteam/platformMovment.cs
@@ -0,0 +1,41 @@
+
+
+using UnityEngine;
+
+public class platformMovment : MonoBehaviour
+{
+    public float moveDistance = 5f;
+    public bool moveOnXAxis = true;
+
+    private Vector3 initialPosition;
+
+    private void Start()
+    {
+        initialPosition = transform.position;
+    }
+
+    private void Update()
+    {
+        if (moveOnXAxis)
+        {
+            MoveOnXAxis();
+        }
+        else
+        {
+            MoveOnYAxis();
+        }
+    }
+
+    private void MoveOnXAxis()
+    {
+        float newX = initialPosition.x + Mathf.PingPong(Time.time * 2f, moveDistance * 2) - moveDistance;
+        transform.position = new Vector3(newX, transform.position.y, transform.position.z);
+    }
+
+    private void MoveOnYAxis()
+    {
+        float newY = initialPosition.y + Mathf.PingPong(Time.time * 2f, moveDistance * 2) - moveDistance;
+        transform.position = new Vector3(transform.position.x, newY, transform.position.z);
+    }
+}
+
