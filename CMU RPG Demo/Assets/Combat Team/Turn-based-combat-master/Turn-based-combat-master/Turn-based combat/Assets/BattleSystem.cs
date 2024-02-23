dfc0a1afca1f9746c37b9379afcd4510f9d988e1
N3utron1 <nathan.hamill17@gmail.com>
2024-02-23 11:18:56 -0500

    Small Update

--- CMU RPG Demo/Assets/Combat Team/Turn-based-combat-master/Turn-based-combat-master/Turn-based combat/Assets/BattleSystem.cs
+++ CMU RPG Demo/Assets/Combat Team/Turn-based-combat-master/Turn-based-combat-master/Turn-based combat/Assets/BattleSystem.cs
@@ -1,13 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine;
+//using UnityEngine.Object;
 
 public enum BattleState { START, PLAYERTURN, ENEMYTURN, WON, LOST }
 
 public class BattleSystem : MonoBehaviour
 {
-	public List<Question> questionDB;
+	//public List<Question> questionDB;
+
+	//public int maxIteration = questionDB.length
 
 	public GameObject playerPrefab;
 	public GameObject enemyPrefab;
@@ -32,6 +35,32 @@ public class BattleSystem : MonoBehaviour
 
 	public BattleState state;
 
+	public Button trueButton;
+	public Text trueButtonText;
+
+	public Button falseButton;
+	public Text falseButtonText;
+
+	//public Button AButton;
+	//public Text AButtonText;
+
+	public Button BButton;
+	public Text BButtonText;
+
+	//public Button CButton;
+	//public Text CButtonText;
+
+	public Button DButton;
+	public Text DButtonText;
+
+	public bool trueFalse;
+
+	public Vector2 truePosition;
+	public Vector2 aPosition;
+
+	public Vector2 falsePosition;
+	public Vector2 cPosition;
+
     void Start()
     {
 		state = BattleState.START;
@@ -47,6 +76,18 @@ public class BattleSystem : MonoBehaviour
 
 		GameObject enemyGO = Instantiate(enemyPrefab, enemyBattleStation);
 		enemyUnit = enemyGO.GetComponent<Unit>();
+
+		//AButton.enabled = false;
+		//AButtonText.enabled = false;
+
+		BButton.enabled = false;
+		BButtonText.enabled = false;
+
+		//CButton.enabled = false;
+		//CButtonText.enabled = false;
+
+		DButton.enabled = false;
+		DButtonText.enabled = false;
 
 		dialogueText.text = "A wild " + enemyUnit.unitName + " approaches...";
 
@@ -123,9 +164,65 @@ public class BattleSystem : MonoBehaviour
 //Player turn function for allowing button use
 	void PlayerTurn()
 	{
+		//Code for if it is a true/false question, or a multiple choice question.
+		/*
+		if(questionDB[iteration].type == "t_f"){
+			trueFalse = true;
+		}else{
+			trueFalse = false;
+		}
+		*/
+		//trueFalse = true;
+
+		if(trueFalse){
+			//Renames
+			trueButtonText.text = "True";
+			falseButtonText.text = "False";
+
+			//Positioning
+			RectTransform trueButtonRectTransform = trueButton.GetComponent<RectTransform>();
+			trueButtonRectTransform.anchoredPosition = truePosition;
+			RectTransform falseButtonRectTransform = falseButton.GetComponent<RectTransform>();
+			falseButtonRectTransform.anchoredPosition = falsePosition;
+
+			//B and D disenabling
+			BButton.enabled = false;
+			BButtonText.enabled = false;
+			DButton.enabled = false;
+			DButtonText.enabled = false;
+		}else{
+			//Renames
+			trueButtonText.text = "A";
+			falseButtonText.text = "C";
+
+			//Suffle Mechanism?
+
+			/*
+			trueButtonText.text = questionDB[iteration].nonAnswers[0];
+			BButtonText.text = questionDB[iteration].nonAnswers[1];
+			falseButtonText.text = questionDB[iteration].nonAnswers[2];
+			DButtonText.text = questionDB[iteration].nonAnswers[3];
+			*/
+
+			//Positioning
+			RectTransform trueButtonRectTransform = trueButton.GetComponent<RectTransform>();
+			trueButtonRectTransform.anchoredPosition = aPosition;
+			RectTransform falseButtonRectTransform = falseButton.GetComponent<RectTransform>();
+			falseButtonRectTransform.anchoredPosition = cPosition;
+
+			//B and D enabling
+			BButton.enabled = true;
+			BButtonText.enabled = true;
+			DButton.enabled = true;
+			DButtonText.enabled = true;
+
+
+		}		
+
 		if(iteration > maxIteration){
 			iteration = 0;
 		}
+
 		//dialogueText.text = "Choose an action:";
 		dialogueText.text = questions[iteration];
 	}
@@ -147,35 +244,43 @@ public class BattleSystem : MonoBehaviour
 //Button controls
 	public void OnTrueButton()
 	{
-		PressButton("True");
+		if(trueFalse){
+			PressButton("True");
+		}else{
+			MultiPressButton(trueButtonText.text);
+		}
 	}
 
 	public void OnFalseButton()
 	{
-		PressButton("False");
+		if(trueFalse){
+			PressButton("False");
+		}else{
+			MultiPressButton(falseButtonText.text);
+		}
 	}
-
+/*
 	public void OnA_Button()
 	{
 		PressButton("A");
 	}
-
+*/
 	public void OnB_Button()
 	{
-		PressButton("B");
+		MultiPressButton(BButtonText.text);
 	}
-
+/*
 	public void OnC_Button()
 	{
 		PressButton("C");
 	}
-
+*/
 	public void OnD_Button()
 	{
-		PressButton("D");
+		MultiPressButton(DButtonText.text);
 	}
 
-//Button function
+//True/False Button function
 	public void PressButton(string buttonValue)
 	{
 		if (state != BattleState.PLAYERTURN)
@@ -190,5 +295,27 @@ public class BattleSystem : MonoBehaviour
 			StartCoroutine(EnemyTurn());
 		}
 	}
+//Multichoice Button function
+		public void MultiPressButton(string buttonValue)
+	{
+		if (state != BattleState.PLAYERTURN)
+			return;
+
+		if(answers[iteration] == buttonValue){
+			iteration = iteration + 1;
+			StartCoroutine(PlayerAttack());
+		}else{
+			iteration = iteration + 1;
+			state = BattleState.ENEMYTURN;
+			StartCoroutine(EnemyTurn());
+		}
+	}
+
+/*
+  if(questionDB[iteration].answer == buttonValue){
+	iteration = iteration + 1;
+	StartCoroutine(PlayerAttack());
+  }
+*/
 
 }
