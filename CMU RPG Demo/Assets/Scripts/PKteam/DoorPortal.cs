56ea929225e46834fd75e9c3608901a0e70d65bb
Louis Jankowski <louisjankowski@gmail.com>
2024-04-15 12:59:15 -0400

    set up the doors

---
+++ CMU RPG Demo/Assets/Scripts/PKteam/DoorPortal.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class DoorPortal : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public string sceneName;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+}
